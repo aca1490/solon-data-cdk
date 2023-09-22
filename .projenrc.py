7e93922dee9e7f2e86470247f4ff131ef8de47c1
Aleksandar Stancic <aleksandar.stancic@helbiz.com>
2023-09-23 01:19:16 +0200

    Update cdk version and change owner

--- .projenrc.py
+++ .projenrc.py
@@ -1,9 +1,10 @@
 from projen.awscdk import AwsCdkPythonApp
 
 project = AwsCdkPythonApp(
-    author_email="aleksandar.stancic@helbiz.com",
+    author_email="tuturutu80@gmail.com",
     author_name="Aleksandar Stancic",
-    cdk_version="2.1.0",
+    cdk_version="2.51.0",
+    cdk_version_pinning=True,
     module_name="solon",
     name="solon",
     version="0.1.0",
